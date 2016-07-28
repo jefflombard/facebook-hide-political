2363e59ca2c183ff7d824a1769aaae19530e74f3
Jeff Lombard <jeffl>
2016-07-28 10:30:17 -0700

    Fixed removing the event listener

--- unfilter.js
+++ unfilter.js
@@ -1,25 +1,3 @@
-function filter_politics(timestamp) {
-    if (!lastRefresh){
-        lastRefresh = 0;
-    }
-
-    var newRefresh = timestamp.timeStamp;
-    console.log(newRefresh - lastRefresh);
-    if (newRefresh - lastRefresh >= 750 || lastRefresh === null){
-        for (var story of stories) {
-            html = story.innerHTML.toLowerCase();
-            for (var term of terms){
-                var regex_term = new RegExp(term);
-                if (regex_term.test(html) === true) {
-                    story.style.display = 'none';
-                }
-            }
-        }
-        lastRefresh = newRefresh;
-    }
-
-}
-
 document.removeEventListener('scroll',filter_politics);
 stories = document.getElementsByClassName('userContentWrapper');
 
